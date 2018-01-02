78d372ad667123bda283a69968ddc6764bfa10fd
laurenmcintire <lamc8684@colorado.edu>
2018-01-02 16:37:55 -0500

    update to GPS class

--- mavimage/GPS.py
+++ mavimage/GPS.py
@@ -1,49 +1,34 @@
-from pymavlink import mavutil
-import sys
-from optparse import OptionParser
-from dronekit import connect, VehicleMode
 
+from dronekit import connect
+'''
+GPS class. Responsible for retrieving GPS data given connection and returning GPS data including lat, long, and time
+'''
 class GPS:
 
-    def __init__(self, connection):
-        parser = OptionParser("readdata.py [options]")
-        parser.add_option("--baudrate", dest="baudrate", type='int',
-                          help="master port baud rate", default=115200)
-        parser.add_option("--device", dest="device", default=None, help="serial device")
-        parser.add_option("--rate", dest="rate", default=4, type='int', help="requested stream rate")
-        parser.add_option("--source-system", dest='SOURCE_SYSTEM', type='int', default=255,
-                          help='MAVLink source system')
-        parser.add_option("--showmessages", dest="showmessages", action='store_true',
-                          help="show incoming messages", default=False)
-        (opts, args) = parser.parse_args()
-
-        if opts.device is None:
-            print("Please select a serial device")
-            sys.exit(1)
-        self.connection = mavutil.mavlink_connection(opts.device, baud=opts.baudrate)
+    def __init__(self, mav_connection):
+        # establish connection attribute
+        self.connection = mav_connection
 
     def register_handlers(self):
+        # return connection
         return self.connection
 
-    def global_position_int_handler(self, message):
-        '''parser = OptionParser(
-            description='Print out vehicle state information. Connects to SITL on local PC by default.')
-        parser.add_option('--connect',
-                            help="vehicle connection target string. If not specified, SITL automatically started and used.")
-        args = parser.parse_args()
-
-        connection_string = args.connect
-        sitl = None
-
-        # Start SITL if no connection string specified
-        if not connection_string:
-            import dronekit_sitl
-            sitl = dronekit_sitl.start_default()
-            connection_string = sitl.connection_string()'''
-        connection_string = self.connection
+    def global_position_int_handler(self, mav_connection):
+        # find gps location and time of UAV
+        connection_string = mav_connection
         vehicle = connect(connection_string, wait_ready=True)
         vehicle.wait_ready('autopilot_version')
-        message = vehicle.gps_0
-        return message
+        # returns the latitude and longitude of the UAV
+        message = vehicle.location.global_frame
+        time = vehicle.time()
+        # need a way to find timestamp
+        #####
+        return message, time
+
+    def record(self):
+        # return the gps record
+        link = self.register_handlers()
+        gps = self.global_position_int_handler(link)
+        return gps
 
 
