f0f605a454bc2f35bf1c5d799bf57a7e885c2945
laurenmcintire <lamc8684@colorado.edu>
2018-01-17 15:34:44 -0700

    test cases

---
+++ tests/test_rpicamera.py
@@ -0,0 +1,9 @@
+from mavimage.camera import Camera
+from mavimage.rpicamera import RPiCamera
+import imghdr
+
+def test_image():
+
+    image = RPiCamera.take_picture()
+    image_type = imghdr.what(image)
+    assert (image_type == 'jpeg')
