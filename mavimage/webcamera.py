9ae260bc4ca06fe1216610e8967cf77dd337490c
laurenmcintire <lamc8684@colorado.edu>
2018-01-18 13:56:51 -0700

    update to core class names

--- mavimage/webcamera.py
+++ mavimage/webcamera.py
@@ -4,13 +4,16 @@ WebCamera class to be inherited by Camera class
 
 """
 
-class WebCamera:
+class WebCamera(Camera):
 
-    def __init__(self, width, height, gps, webcamera):
+    def __init__(self, webcamera, gps):
         # width, height, gps:GPS, webcamera:VideoCapture.device
         super().__init__(gps)
         self._webcamera = Camera()
 
     def take_picture(self):
         img = self._webcamera.getImage()
+        return img
+
+
 
