58a2862d22a9dff1be4c0a4a365d9cb3020692dc
Lauren McIntire <lamc8684@colorado.edu>
2018-02-01 15:57:52 -0700

    Initial code for to_bytes, from_bytes, exif_to_gps, and gps_to_exif

---
+++ mavimage/image.py
@@ -0,0 +1,45 @@
+import Pillow
+import piexif
+import io
+"""
+Image class.
+Attributes:
+    image:PIL.Image
+    gps:GPSRecord
+"""
+
+class Image:
+
+    def __init__(self, image=None, gps=None):
+        """image: PIL.Image, gps:GPSRecord"""
+        self.gps = gps
+        self.image = image
+
+    def from_bytes(self, bytes, format=None):
+        """constuct image from bytes into format. bytes string"""
+        gps = exif_to_gps(bytes)
+        stream = io.BytesIO(bytes)
+        image = Pillow.Image.open(stream)
+        webp = Pillow.Image.frombytes(image)
+        piexif.insert(gps, image)
+
+    def to_bytes(self, format=None):
+        """constuct image to bytes from format (webp, jpeg). bytes string"""
+        dict = gps_to_exif(self.gps)
+        bytes_image = io.BytesIO()
+        self.image.save(bytes_image, format=format)
+        piexif.insert(dict, bytes_image, bytes)
+        return bytes
+
+    def save(self, fp, format=None):
+        """save to fp pointer(name or open file object)"""
+
+def gps_to_exif(self, gps_record):
+    """gps_record: GPSRecord. Turn GPS record to exif"""
+    dict = dump(gps_record)
+    return dict
+
+def exif_to_gps(self, exif):
+    """"exif: dict. Turn exif to GPS record"""
+    GPSRecord = load(exif)
+    return GPSRecord
--- mavimage/photographer.py
+++ mavimage/photographer.py
@@ -7,23 +7,17 @@ Attributes: camera:Camera, image_sender:ImageSender, image_writer:ImageWriter
 class Photographer:
 
     def __init__(self, camera, image_sender, image_writer=None):
-        # camera: PiCamera, image_sender:ImageSender, image_writer:ImageWriter
+        """camera: PiCamera, image_sender:ImageSender, image_writer:ImageWriter"""
         self.image_sender = image_sender
-        self.image_writer = image_writer
         self.camera = camera
 
     def register_handlers(self, mavlink):
-        #what is this supposed to do?
-        # mavlink: MAVLinkConnection
-        return mavlink
+        """Pass mavlink: MAVLinkConnection to call push_handler function"""
+        mavlink.push_handler('DATA_TRANSMISSION_HANDSHAKE', self.data_transmission_handshake_handler)
 
     def data_transmission_handshake_handler(self, mavlink, message):
-        # mavlink:MavLinkConnection, message:str
-        # return a bool, True if heartbeat message?
-        msg_type = message.get_type()
-        if msg_type == "HEARTBEAT":
-            return True
-        else:
-            return False
+        """mavlink:MavLinkConnection, message:str"""
+        pass
+
 
 
