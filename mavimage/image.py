27c97d702eceea1c4228c3f7f3c9ced7db64ec3d
Lauren McIntire <lamc8684@colorado.edu>
2018-02-06 12:28:40 -0700

    added docstrings to functions. Rewrote logic for to_bytes function

--- mavimage/image.py
+++ mavimage/image.py
@@ -1,4 +1,4 @@
-import Pillow
+from PIL import Image, ExifTags
 import piexif
 import io
 """
@@ -12,34 +12,45 @@ class Image:
 
     def __init__(self, image=None, gps=None):
         """image: PIL.Image, gps:GPSRecord"""
-        self.gps = gps
         self.image = image
+        self.gps = gps
 
-    def from_bytes(self, bytes, format=None):
-        """constuct image from bytes into format. bytes string"""
-        gps = exif_to_gps(bytes)
-        stream = io.BytesIO(bytes)
-        image = Pillow.Image.open(stream)
-        webp = Pillow.Image.frombytes(image)
-        piexif.insert(gps, image)
+    @classmethod
+    def from_bytes(image_bytes):
+        """construct image from bytes into format. bytes string. return Image"""
+        """Turn into BytesIO"""
+        stream = io.BytesIO(image_bytes)
+        """open the stream """
+        image = Image.open(stream)
+        """Extract GPS data image_bytes and convert it to GPSRecord"""
+        gps_bytes = piexif.load(image.info["exif"])
+        gps = exif_to_gps(gps_bytes)
+        """Transform into image of format with exif data"""
+        # webp = Image.Image.frombytes(image)
+        wepb = image.save(file, "WEBP", exif=gps)
 
-    def to_bytes(self, format=None):
-        """constuct image to bytes from format (webp, jpeg). bytes string"""
-        dict = gps_to_exif(self.gps)
-        bytes_image = io.BytesIO()
-        self.image.save(bytes_image, format=format)
-        piexif.insert(dict, bytes_image, bytes)
-        return bytes
+    def to_bytes(self):
+        """construct image to bytes from format (webp, jpeg). bytes string"""
+        """Create exif from given GPS record"""
+        gps_record = gps_to_exif(self.gps)
+        """open image and convert into bytes"""
+        with open(self.image) as image_stream:
+            f = image_stream.read()
+            b = bytearray(f)
+        """Attach exif data"""
+        piexif.insert(gps_record, b)
+        return b
 
-    def save(self, fp, format=None):
-        """save to fp pointer(name or open file object)"""
+    def save(self, fp):
+        """save to fp pointer(name(string) or open file object(BytesIO))"""
+        Image.Image.save(fp, format='WEBP', paramters=self.gps)
 
 def gps_to_exif(self, gps_record):
     """gps_record: GPSRecord. Turn GPS record to exif"""
-    dict = piexif.dump(gps_record)
-    return dict
+    dict_record = piexif.dump(gps_record)
+    return dict_record
 
 def exif_to_gps(self, exif):
     """"exif: dict. Turn exif to GPS record"""
-    GPSRecord = piexif.load(exif)
-    return GPSRecord
+    gps_record = piexif.load(exif)
+    return gps_record
