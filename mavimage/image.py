bc7d5073e5aecc001fda7eb1cc93fa98991b155d
Lauren McIntire <lamc8684@colorado.edu>
2018-02-15 15:32:53 -0700

    Fix to work with fractional GPS coords, and with WebP formats Style upgrade Testing helper functions

--- mavimage/image.py
+++ mavimage/image.py
@@ -1,19 +1,22 @@
+"""Handle Image class and helper functions
+Lauren McIntire"""
+
+from fractions import Fraction
+from datetime import datetime
+import io
 import PIL.Image
 import piexif
-import io
 from .gps import GPSRecord
-from datetime import datetime
-from fractions import Fraction
-
-"""
-Image class.
-Attributes:
-    image:PIL.Image
-    gps:GPSRecord
-"""
 
 
 class Image:
+    """
+    Image class.
+    Attributes:
+    image:PIL.Image
+    gps:GPSRecord
+    Manage combining images with GPS coordinates
+    can handle JPEG and WebP formats"""
 
     def __init__(self, image=None, gps=None):
         """image: PIL.Image, gps:GPSRecord"""
@@ -22,18 +25,22 @@ class Image:
 
     @classmethod
     def from_bytes(cls, image_bytes):
-        """construct image from bytes into format. bytes string. return Image
+        """ Input: image_bytes: bytes object
+        Output: Image: Image class
+        construct image from bytes into format. bytes string. return Image
         Turn into BytesIO
         open the stream
         Extract GPS data image_bytes and convert it to GPSRecord
         Transform into image of format with exif data"""
         stream = io.BytesIO(image_bytes)
         image = PIL.Image.open(stream)
-        gps = exif_to_gps(image_bytes)
+        gps = exif_to_gps(image.info.get('exif'))
         return Image(image, gps)
 
     def to_bytes(self, given_format):
-        """construct image to bytes from self.image (PIL.Image). bytes string. Save as BytesIO object
+        """ Input: given_format: string
+        Output: bytes
+        construct image to bytes from self.image (PIL.Image). bytes string. Save as BytesIO object
         Create exif from given GPS record
         open image and convert into bytesIO"""
         output = io.BytesIO()
@@ -42,15 +49,16 @@ class Image:
         return output.read()
 
     def save(self, fp, given_format):
-        """open file object(BytesIO))"""
+        """Input: fp: BytesIO object, given_format: string
+        open file object(BytesIO)), insert exif bytes, and save to file pointer"""
         gps_dict = gps_to_exif(self._gps)
-        dict_whole = {"0th": {}, "Exif": {}, "GPS": gps_dict,
-                      "Interop": {}, "thumbnail": None}
-        exif_bytes = piexif.dump(dict_whole)
+        exif_bytes = piexif.dump(gps_dict)
         self._image.save(fp, format=given_format, exif=exif_bytes)
 
 def gps_to_exif(gps_record):
-    """gps_record: GPSRecord. Turn GPS record to exif"""
+    """Input: gps_record: GPSRecord.
+    Output: dict_whole: dictionary with exif data
+    Turn GPS record to exif"""
     gps_dict = {}
     gps_dict[piexif.GPSIFD.GPSLongitudeRef] = b'E' if gps_record.longitude >= 0 else b'W'
     long_deg, long_min, long_sec = deg2dms(abs(gps_record.longitude))
@@ -63,11 +71,11 @@ def gps_to_exif(gps_record):
     gps_dict[piexif.GPSIFD.GPSLatitudeRef] = b'N' if gps_record.latitude >= 0 else b'S'
     lat_deg, lat_min, lat_sec = deg2dms(abs(gps_record.latitude))
     gps_dict[piexif.GPSIFD.GPSLatitude] = ((Fraction(lat_deg).limit_denominator().numerator,
-                                             Fraction(lat_deg).limit_denominator().denominator),
-                                            (Fraction(lat_min).limit_denominator().numerator,
-                                             Fraction(lat_min).limit_denominator().denominator),
-                                            (Fraction(lat_sec).limit_denominator().numerator,
-                                             Fraction(lat_sec).limit_denominator().denominator))
+                                            Fraction(lat_deg).limit_denominator().denominator),
+                                           (Fraction(lat_min).limit_denominator().numerator,
+                                            Fraction(lat_min).limit_denominator().denominator),
+                                           (Fraction(lat_sec).limit_denominator().numerator,
+                                            Fraction(lat_sec).limit_denominator().denominator))
     gps_dict[piexif.GPSIFD.GPSAltitudeRef] = 0 if gps_record.altitude >= 0 else 1
     gps_dict[piexif.GPSIFD.GPSAltitude] = (int(abs(gps_record.altitude)), 1)
     gps_dict[piexif.GPSIFD.GPSMapDatum] = b'WGS-84'
@@ -75,11 +83,15 @@ def gps_to_exif(gps_record):
                                             (Fraction(gps_record.time.second).limit_denominator().numerator,
                                              Fraction(gps_record.time.second).limit_denominator().denominator))
     gps_dict[piexif.GPSIFD.GPSDateStamp] = gps_record.time.strftime('%Y:%m:%d')
-    return gps_dict
+    dict_whole = {"0th": {}, "Exif": {}, "GPS": gps_dict,
+                  "Interop": {}, "thumbnail": None}
+    return dict_whole
 
 
 def exif_to_gps(exif):
-    """"exif: dict. Turn exif to GPS record"""
+    """"Turn exif to GPS record
+    Input: exif: dict
+    Output: gps_record: GPSRecord"""
     gps_exif = piexif.load(exif)
     gps_exif = gps_exif["GPS"]
     time = datetime.strptime(gps_exif[piexif.GPSIFD.GPSDateStamp].decode("utf-8"), '%Y:%m:%d')
@@ -90,9 +102,12 @@ def exif_to_gps(exif):
                                   gps_exif[piexif.GPSIFD.GPSLatitude][1][0]/gps_exif[piexif.GPSIFD.GPSLatitude][1][1],
                                   gps_exif[piexif.GPSIFD.GPSLatitude][2][0]/gps_exif[piexif.GPSIFD.GPSLatitude][2][1])
     sign_long = -1 if gps_exif[piexif.GPSIFD.GPSLongitudeRef] == b'W' else 1
-    longitude = sign_long * dms2deg(gps_exif[piexif.GPSIFD.GPSLongitude][0][0]/gps_exif[piexif.GPSIFD.GPSLongitude][0][1],
-                                    gps_exif[piexif.GPSIFD.GPSLongitude][1][0]/gps_exif[piexif.GPSIFD.GPSLongitude][1][1],
-                                    gps_exif[piexif.GPSIFD.GPSLongitude][2][0]/gps_exif[piexif.GPSIFD.GPSLongitude][2][1])
+    longitude = sign_long * dms2deg(gps_exif[piexif.GPSIFD.GPSLongitude][0][0] /
+                                    gps_exif[piexif.GPSIFD.GPSLongitude][0][1],
+                                    gps_exif[piexif.GPSIFD.GPSLongitude][1][0] /
+                                    gps_exif[piexif.GPSIFD.GPSLongitude][1][1],
+                                    gps_exif[piexif.GPSIFD.GPSLongitude][2][0] /
+                                    gps_exif[piexif.GPSIFD.GPSLongitude][2][1])
     sign_alt = 1 if gps_exif[piexif.GPSIFD.GPSAltitudeRef] == 0 else -1
     altitude = sign_alt * gps_exif[piexif.GPSIFD.GPSAltitude][0]/gps_exif[piexif.GPSIFD.GPSAltitude][1]
     gps_record = GPSRecord(time, latitude, longitude, altitude)
@@ -100,12 +115,18 @@ def exif_to_gps(exif):
 
 
 def deg2dms(degrees):
+    """Convert degrees to Degrees, Minutes, Seconds
+    Input: degrees
+    Output: deg(degrees), mins(degrees minutes), sec(degrees sec)"""
     deg = int(degrees)
     minutes = int((degrees-deg)*60)
     sec = round(((((degrees - deg)*60)-minutes)*60), 8)
     return deg, minutes, sec
 
 
-def dms2deg(d, m, s):
-    degree = round((s/60+m)/60+d, 8)
+def dms2deg(deg, mins, sec):
+    """Convert Degrees, Minutes, Seconds to degrees up to 8 decimal places
+    Input: deg(degrees), mins(degrees minutes), sec(degrees sec)
+    Output: degrees"""
+    degree = round((sec/60+mins)/60+deg, 8)
     return degree
--- tests/test_image.py
+++ tests/test_image.py
@@ -1,9 +1,10 @@
-from mavimage.image import Image
+from mavimage.image import Image, gps_to_exif, exif_to_gps, deg2dms, dms2deg
 from mavimage.gps import GPS, GPSRecord
 import pytest
 from pytest_mock import mocker
 import PIL.Image
 from datetime import datetime
+import piexif
 
 
 def test_init():
@@ -12,15 +13,6 @@ def test_init():
     new_image = Image(im, gps)
     assert (new_image._image == im)
     assert (new_image._gps == gps)
-
-"""def test_to_bytes():
-    im = PIL.Image.new('L', (3, 3))
-    time = datetime(1, 1, 1, 1, 1, 1)
-    gps = GPSRecord(time, 1, 1, 100)
-    new_image = Image(im, gps)
-    image_bytes = new_image.to_bytes('WebP')
-    assert (type(image_bytes) == bytes)
-"""
 
 def test_from_bytes():
     im = PIL.Image.new('L', (4, 4))
@@ -53,11 +45,36 @@ def test_from_bytes():
     time = datetime(2018, 2, 9, 13, 21, 30)
     gps = GPSRecord(time, -40.15, 100.2, -100)
     new_image = Image(im, gps)
-    image_bytes = new_image.to_bytes('jpeg')
+    image_bytes = new_image.to_bytes('webp')
     image_from_byte = new_image.from_bytes(image_bytes)
-    image_compare = image_from_byte.to_bytes('jpeg')
+    image_compare = image_from_byte.to_bytes('webp')
     assert(image_bytes == image_compare)
     assert(image_from_byte._gps.altitude == new_image._gps.altitude)
     assert (image_from_byte._gps.longitude == new_image._gps.longitude)
     assert (image_from_byte._gps.latitude == new_image._gps.latitude)
     assert (image_from_byte._gps.time == new_image._gps.time)
+
+def test_exif_to_gps():
+    time = datetime(2000, 9, 18, 20, 17, 30)
+    gps = GPSRecord(time, 40.15, -105.201, 4000)
+    dictionary = gps_to_exif(gps)
+    dictionary = piexif.dump(dictionary)
+    gps_compare = exif_to_gps(dictionary)
+    assert(gps.longitude == gps_compare.longitude)
+    assert (gps.latitude == gps_compare.latitude)
+    assert (gps.altitude == gps_compare.altitude)
+    assert (gps.time == gps_compare.time)
+
+def test_dms2deg():
+    degree1 = 100
+    degree2 = -3.1415
+    degree3 = -900
+    d1, m1, s1 = deg2dms(degree1)
+    degree1_compare = dms2deg(d1, m1, s1)
+    assert (degree1_compare == degree1)
+    d2, m2, s2 = deg2dms(degree2)
+    degree2_compare = dms2deg(d2, m2, s2)
+    assert (degree2_compare == degree2)
+    d3, m3, s3 = deg2dms(degree3)
+    degree3_compare = dms2deg(d3, m3, s3)
+    assert (degree3_compare == degree3)
