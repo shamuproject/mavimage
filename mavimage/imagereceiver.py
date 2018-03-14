7bfc2351b7993c95ee9d36009e71ba1a2d39c387
Lauren McIntire <lamc8684@colorado.edu>
2018-03-14 17:45:26 -0600

    Update protocol to send only missing packets

--- mavimage/imagereceiver.py
+++ mavimage/imagereceiver.py
@@ -1,7 +1,6 @@
 from multiprocessing import Lock
 from .chunkedbytes import ChunkedBytes
 
-
 class ImageReceiver:
     """ImageReceiver class
     Attributes:
@@ -9,15 +8,19 @@ class ImageReceiver:
         image: ChunkedBytes
         received_chunks: int
     """
-    def __init__(self):
+    def __init__(self, wait_time=5):
          self._received_chunks = []
          self._mutex = Lock()
          self._image = ChunkedBytes(None, 253)
+         self._wait_time = wait_time
 
     def receive(self, mavlink):
-        # mavlink: MavLinkConnection, image:Image
+        """Args:
+            mavlink: MavLinkConnection
+            image: Image
+        """
         self.register_handlers(mavlink)
-        mavlink.send_data_transmission_handshake(type="WEBP", size=0, packets=0, payload=0)
+        mavlink.data_transmission_handshake_send(type="MAVLINK_DATA_STREAM_IMG_JPEG", size=0, width=0, height=0, packets=0, payload=0, jpg_quality=0, force_mavlink1=False)
 
     def register_handlers(self, mavlink):
         # mavlink: MavLinkConnection
@@ -35,15 +38,24 @@ class ImageReceiver:
     def encapsulated_data_handler(self, mavlink, message):
         # mavlink: MAVLinkConnection, message: MAVLink_encapsulated_data_message
         # handle receiving encapsulated data bytes, put into chunkedbytes
+
+        # timer for few seconds -- then send request for packets don't have
+        # recursively erase received packets
+        mavlink.add_timer(5, self._data_ack)
         self._received_chunks.append(message.seqnr)
         if self._image.flat() == None:
             self._image._bytes_item = message.data
         else:
-            self._image = self._image + message.data
-        if message.seqnr % 127 == 0:
-            self._data_ack_register(mavlink, self._received_chunks)
+            self._image + message.data
 
-    def _data_ack_register(self, mavlink, received):
-        mavlink.data_ack_send(127, received)
-
+    def _data_ack(self, mavlink, received):
+        sorted_received = sorted(received)
+        missing = []
+        j = 0
+        for i in range(1,self.packets):
+            if sorted_received[i-1] != j:
+                missing.append(received[i-1])
+            else:
+                j = j+1
+        mavlink.data_ack_send(len(missing), missing)
 
--- tests/test_imagereceiver.py
+++ tests/test_imagereceiver.py
@@ -14,6 +14,11 @@ class Message_EncapData:
         self.seqnr = 0
         self.data = b'abc'
 
+class Message_EncapData3:
+    def __init__(self):
+        self.seqnr = 1
+        self.data = b'abc'
+
 class Message_EncapData2:
     def __init__(self):
         self.seqnr = 127
@@ -27,6 +32,32 @@ class MockMav:
         self.handlers[message] = function
 
     def data_ack_send(self, packets, received):
+        self.push_handler("DATA_ACK", self.data_ack_send)
+
+    def data_transmission_handshake_send(self, type, size, width, height, packets, payload, jpg_quality,
+                                         force_mavlink1=False):
+        '''
+
+
+        type                      : type of requested/acknowledged data (as defined in ENUM DATA_TYPES in mavlink/include/mavlink_types.h) (uint8_t)
+        size                      : total data size in bytes (set on ACK only) (uint32_t)
+        width                     : Width of a matrix or image (uint16_t)
+        height                    : Height of a matrix or image (uint16_t)
+        packets                   : number of packets beeing sent (set on ACK only) (uint16_t)
+        payload                   : payload size per packet (normally 253 byte, see DATA field size in message ENCAPSULATED_DATA) (set on ACK only) (uint8_t)
+        jpg_quality               : JPEG quality out of [1,100] (uint8_t)
+
+        '''
+        pass
+
+    def encapsulated_data_send(self, seqnr, data, force_mavlink1=False):
+        '''
+
+
+        seqnr                     : sequence number (starting with 0 on every transmission) (uint16_t)
+        data                      : image data bytes (uint8_t)
+
+        '''
         pass
 
 def test_init():
@@ -51,25 +82,33 @@ def test_data_transmission_handshake_handler():
     assert receiver.payload == 253
     assert receiver.packets == 1
 
-def test_encapsulated_data_handler():
+def test_encapsulated_data_handler(mocker):
     mav = MockMav()
     receiver = ImageReceiver()
     message = Message_EncapData()
     receiver.encapsulated_data_handler(mav, message)
     assert receiver._image.flat() == b'abc'
     assert receiver._received_chunks == [0]
+    message2 = Message_EncapData3()
+    receiver.encapsulated_data_handler(mav, message2)
+    assert receiver._image.flat() == b'abcabc'
+    assert receiver._received_chunks == [0, 1]
 
     mav = MockMav()
+    mocker.patch.object(MockMav, 'data_ack_send')
     message = Message_EncapData2()
     receiver = ImageReceiver()
     receiver.encapsulated_data_handler(mav, message)
     assert receiver._image.flat() == b'abc'
     assert receiver._received_chunks == [127]
+    MockMav.data_ack_send.assert_called_with(127, [127])
 
     mav = MockMav()
-    message = Message_EncapData3()
     receiver = ImageReceiver()
-    receiver.encapsulated_data_handler(mav, message)
-    mav.handlers["DATA_TRANSMISSION_HANDSHAKE"](mav, message)
-    assert receiver._image.flat() == b'abc'
-    assert receiver._received_chunks == [0,1]
+    receiver.receive(mav)
+    assert "ENCAPSULATED_DATA" in mav.handlers
+    assert mav.handlers["ENCAPSULATED_DATA"] == receiver.encapsulated_data_handler
+    assert "DATA_TRANSMISSION_HANDSHAKE" in mav.handlers
+    assert mav.handlers["DATA_TRANSMISSION_HANDSHAKE"] == receiver.data_transmission_handshake_handler
+
+
