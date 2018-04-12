c00fb9df1f4c1781bcf4a7234eb0e756ce36d60d
Lauren McIntire <lamc8684@colorado.edu>
2018-04-12 16:41:09 -0600

    clean-up

--- mavimage/senderapp.py
+++ mavimage/senderapp.py
@@ -1,13 +1,13 @@
-
-'''
+"""
 SenderAPP class. Inherited by App class
 Attributes:
     mavlink:MAVLinkConnection
     photographer:Photographer
     logger:Logger=None
-'''
+"""
 
 class SenderApp:
 
     def __init__(self, mavlink, photographer, logger=None):
         self.logger = logger
+        self.photographer = photographer
