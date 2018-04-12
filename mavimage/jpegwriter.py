eeef843194f66eca425baffdbaaad8f565b05c24
Lauren McIntire <lamc8684@colorado.edu>
2018-04-12 16:48:05 -0600

    clean-up

--- mavimage/jpegwriter.py
+++ mavimage/jpegwriter.py
@@ -1,5 +1,4 @@
 from .imagewriter import ImageWriter
-
 """
 JPEGWriter class
 Attributes:
@@ -10,9 +9,17 @@ Attributes:
 class JPEGWriter(ImageWriter):
 
     def __init__(self, path_format):
+        """Initialize JPEG writer
+        Args:
+            path_format: str
+        """
         super().__init__(path_format)
 
     def write(self, image):
+        """write image to file with date
+        Args:
+            image:Image
+        """
         date = image._gps.time.strftime('%m-%d-%Y-%H-%M-%S')
         name = '{}{}{}{}'.format(self.path, '/', date, '.jpeg')
         image.save(name, given_format='jpeg')
